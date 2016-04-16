99c15e70362d80f52bb3916037181b80690ea449
CC3 <cc3.staff@gmail.com>
2016-04-16 17:56:12 -0600

    Archivos proyecto 3

---
+++ calcDepthOptimized.h
@@ -0,0 +1,5 @@
+// CC3 2016 Proyecto 3
+
+#pragma once
+
+void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth, int imageHeight, int featureWidth, int featureHeight, int maximumDisplacement);
